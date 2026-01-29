84a7c89c910f2ec63ab8a0bdaaa4b8ea05f280b8
BiPOS <contato@bipos.com.br>
2026-01-29 14:24:51 -0300

    update module initializer (pos-api)

--- src/main/kotlin/br/com/bipos/smartposapi/SmartPosApplication.kt
+++ src/main/kotlin/br/com/bipos/smartposapi/SmartPosApplication.kt
@@ -8,7 +8,9 @@ import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EntityScan(basePackages = ["br.com.bipos.smartposapi"])
 @EnableConfigurationProperties(PosJwtProperties::class)
+@EnableJpaRepositories(basePackages = ["br.com.bipos.smartposapi"])
 class SmartPosApplication
 
 fun main(args: Array<String>) {
